9d6e799d62cef26c09050f505ec069339c5a0464
kshitij-dubey-001 <kshitijdubey2007@gmail.com>
2026-01-07 13:57:14 +0530

    add click counter using dom

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function changeText() {
   document.getElementById("title").innerText = "Text Changed!";
 }
+let count = 0;
+
+function increase() {
+  count++;
+  document.getElementById("count").innerText = count;
+}
+
